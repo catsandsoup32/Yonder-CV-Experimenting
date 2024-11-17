1764ec55fd643cf0bc547cbd12875fc2d04983d7
Edmund Bu <2edmundbu@gmail.com>
2024-11-17 13:39:12 -0800

    gitignore datasets for PC

--- synthetic_pipeline/diffusion_gen.py
+++ synthetic_pipeline/diffusion_gen.py
@@ -1,7 +1,9 @@
 from diffusers import StableDiffusionPipeline
 import torch
+import matplotlib.pyplot as plt
 
 pipeline = StableDiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16).to("cuda")
-pipeline.load_textual_inversion(r"C:\Users\edmun\VSC_DIRS\Yonder-CV-Experimenting\synthetic_pipeline\textual_inversion_weights\bottle1_step3000_not_good.safetensors")
-image = pipeline("a <wide-mouth-bottle> in a desert", num_inference_steps=50).images[0]
-image.save("bottle-train.png")
+pipeline.load_textual_inversion(r"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\synthetic_pipeline\textual_inversion_weights\mallet1_step500.safetensors")
+image = pipeline("a <orange-mallet> in a desert", num_inference_steps=50).images[0]
+plt.imshow(image)
+plt.show()
