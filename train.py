5bb36dafddfb1e0823b2e3f69c04753344ebd97e
Edmund Bu <2edmundbu@gmail.com>
2024-11-17 21:49:45 -0800

    Move to pc

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 model = YOLO("pretrained_weights/yolo11s.pt")
 
 output = model.train(
-    data="datasets/baseline_data/data.yaml", 
-    epochs=10,
+    data="datasets/sc_uni_dataset/data.yaml", 
+    epochs=150,
     batch=16,
     imgsz=640,
     device='cuda',
