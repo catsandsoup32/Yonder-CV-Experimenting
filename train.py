c2d96d711825c1d2a07d65e6ddc23ffe36fd46cb
catsandsoup32 <edmundpenguin@gmail.com>
2024-11-19 00:19:11 -0800

    First training with SC dataset

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 model = YOLO("pretrained_weights/yolo11s.pt")
 
 output = model.train(
-    data="datasets/sc_uni_dataset/data.yaml", 
+    data="datasets/sc_data/data.yaml", 
     epochs=150,
     batch=16,
     imgsz=640,
     device='cuda',
-    workers=0
+    workers=0,
 )
