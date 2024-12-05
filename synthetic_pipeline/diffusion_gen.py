22f76f93a024886c387e6ec864578f9581d726ba
Edmund Bu <2edmundbu@gmail.com>
2024-12-04 23:33:39 -0800

    test done

--- synthetic_pipeline/diffusion_gen.py
+++ synthetic_pipeline/diffusion_gen.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 
 pipeline = StableDiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16).to("cuda")
-pipeline.load_textual_inversion(r"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\synthetic_pipeline\textual_inversion_weights\bottle1_step3000_not_good.safetensors")
-image = pipeline("a photo of a <wide-mouth-bottle>", num_inference_steps=50).images[0]
+pipeline.load_textual_inversion(r"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\synthetic_pipeline\textual_inversion_weights\learned_embeds-steps-3000.safetensors")
+image = pipeline("a photo of a <orange-mallet>", num_inference_steps=50).images[0]
 plt.imshow(image)
 plt.show()
