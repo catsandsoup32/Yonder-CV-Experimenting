c61512ac0896d7a496ead8da5409693f77c81236
Edmund Bu <2edmundbu@gmail.com>
2025-01-11 23:42:16 -0800

    trained with random null images

--- train.py
+++ train.py
@@ -2,11 +2,13 @@ from ultralytics import YOLO
 
 model = YOLO("pretrained_weights/yolo11s.pt")
 
-output = model.train(
-    data="datasets/sc_data/data.yaml", 
-    epochs=150,
-    batch=16,
-    imgsz=640,
-    device='cuda',
-    workers=0,
-)
+if __name__ == '__main__':
+    output = model.train(
+        data=r"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\datasets\sc_uni_dataset\data.yaml", 
+        epochs=150,
+        batch=16,
+        imgsz=640,
+        device='cuda',
+        workers=10,
+        patience=15
+    )
