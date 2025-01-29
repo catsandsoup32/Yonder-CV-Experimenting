fc822b2ae7ff3aaf640cbc5e6efd8c5343657fb6
catsandsoup32 <edmundpenguin@gmail.com>
2025-01-28 22:40:35 -0800

    add train6 lr=1e-5 save file

--- process_extra.py
+++ process_extra.py
@@ -3,20 +3,20 @@ from pathlib import Path
 import shutil
 import os
 
-new_dir = Path(r"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\datasets\val2017")
+new_dir = Path(r"C:\Users\edmun\VSC_DIRS\Yonder-CV-Experimenting\datasets\val2017")
 
 def add_coco_data(new_dir):
     
     for idx, file in enumerate(new_dir.glob("*")):
         file_name = str(file).split("\\")[-1].removesuffix(".jpg") + ".txt"
 
-        if idx < 750: save_dir = 'train' 
-        elif idx < 1000: save_dir = 'valid'
+        if idx < 300: save_dir = 'train' 
+        elif idx < 400: save_dir = 'valid'
         else: break
             #save_dir = 'test'
 
-        shutil.copy(file, rf"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\datasets\sc_uni_dataset\{save_dir}\images")
-        with open(rf"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\datasets\sc_uni_dataset\{save_dir}\labels\{file_name}", 'w') as file: pass
+        shutil.copy(file, rf"C:\Users\edmun\VSC_DIRS\Yonder-CV-Experimenting\datasets\january_data\{save_dir}\images")
+        with open(rf"C:\Users\edmun\VSC_DIRS\Yonder-CV-Experimenting\datasets\january_data\{save_dir}\labels\{file_name}", 'w') as file: pass
         print(idx)
 
 def loop_remove(data_path, dir_type):
@@ -43,7 +43,7 @@ def add_test_data_to_train(test_path, train_path):
     
 
 if __name__ == '__main__':
-    #add_coco_data(new_dir)
+    add_coco_data(new_dir)
     #remove_all_coco_imgs(Path(r"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\datasets\sc_uni_dataset"))
     pass        
         
--- train.py
+++ train.py
@@ -1,15 +1,25 @@
 from ultralytics import YOLO
 
-model = YOLO("pretrained_weights/yolo11m.pt")
+model = YOLO("pretrained_weights/yolo11s.pt")
 
 if __name__ == '__main__':
     output = model.train(
-        data=r"C:\Users\edmun\Desktop\VSCode Projects\Yonder\Yonder-CV-Experimenting\datasets\sc_uni_dataset\data.yaml", 
-        epochs=300,
+        data=r"C:\Users\edmun\VSC_DIRS\Yonder-CV-Experimenting\datasets\january_data\data.yaml", 
+        epochs=1000,
         batch=16,
         imgsz=640,
         device='cuda',
-        workers=10,
-        lr0=1e-6,
-        patience=15
+        workers=8,
+        optimizer='AdamW',
+        lr0=1e-5,
+        patience=20,
+        pretrained=True
     )
+
+# Training notes
+
+# YoloV11 with january dataset, 300/100 COCO in train/val
+# auto -> train5
+
+# YoloV11 with january dataset, 300/100 COCO in train/val
+# 1e-5 -> train6
